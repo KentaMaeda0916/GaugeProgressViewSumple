4ba03a4784f50ca82772468a3c072b1e48974d9c
maeken <67748136+KentaMaeda0916@users.noreply.github.com>
2025-01-08 22:25:33 +0900

    Initial Commit

---
+++ GaugeProgressViewSumple/GaugeProgressViewSumpleApp.swift
@@ -0,0 +1,17 @@
+//
+//  GaugeProgressViewSumpleApp.swift
+//  GaugeProgressViewSumple
+//
+//  Created by KentaMaeda on 2025/01/08.
+//
+
+import SwiftUI
+
+@main
+struct GaugeProgressViewSumpleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
